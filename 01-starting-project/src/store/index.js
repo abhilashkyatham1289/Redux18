44d32e1bf40dcf49bd0376bd2910c293bdbd7a0a
Abhilash <abhilash.kyatham@gmail.com>
2021-10-27 23:07:34 +0530

    first commit

---
+++ 01-starting-project/src/store/index.js
@@ -0,0 +1,68 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from './counter';
+import authReducer from './auth';
+
+// const initialState = { counter: 0, showCounter: true };
+
+// const counterSlice = createSlice({
+//   name: 'counter',
+//   initialState,
+//   reducers: {
+//     increment(state) {
+//       state.counter++;
+//     },
+//     decrement(state) {
+//       state.counter--;
+//     },
+//     increase(state, action) {
+//       state.counter = state.counter + action.payload;
+//     },
+//     toggleCounter(state) {
+//       state.showCounter = !state.showCounter;
+//     },
+//   },
+// });
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "toggle") {
+//     return {
+//       counter: state.counter,
+//       showCounter: !state.showCounter,
+//     };
+//   }
+//   return state;
+// };
+
+//const store = createStore(counterReducer); we can use combine store also from useRedux instead configureStore
+
+//const store = configureStore({
+ // reducer : counterSlice.reducer
+//});
+
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
+
+//export const counterActions = counterSlice.actions;
+export default store;
